b7624d7034090af3c38a59c887e46d6f3519e557
Tiago Galdino <tiagogaldino@gmail.com>
2022-07-14 18:49:00 -0300

    finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import usersRoute from './routes/users.route';
+import statusRoute from './routes/status.route';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(usersRoute);
+
 app.listen(3000, () => {
-    console.log('Aplicação execuntando na porta 3000!');
+    console.log('Aplicação executando na porta 3000!');
 })
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso total!'});
-});
+app.use(statusRoute);
